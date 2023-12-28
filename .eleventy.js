1d6f4ce6da3948d1bf6a378628f1c16d4b026654
Greg Tyler <gregtyler@users.noreply.github.com>
2023-12-28 13:13:14 +0000

    Convert one article to Markdown (#12)
    Demonstrates that this works, having now build all the requisite parts

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,39 @@
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("assets");
-  eleventyConfig.addPassthroughCopy("images");
+const site = require("./_data/site");
+const { DateTime } = require("luxon");
+const markdownit = require("markdown-it");
+
+const md = markdownit();
+
+module.exports = function (config) {
+  config.addPassthroughCopy("assets");
+  config.addPassthroughCopy("images");
+
+  config.addFilter("date", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_FULL, {
+      locale: "en-GB",
+    });
+  });
+
+  config.addFilter("dateISO", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toISODate();
+  });
+
+  config.addFilter("toAbsoluteUrl", (url) => {
+    return new URL(url, site.baseUrl).href;
+  });
+
+  config.addPairedShortcode("figure", (content, caption = "", align = "") => {
+    let classes = "c-figure";
+    if (align === "left") classes += " c-figure--left";
+    if (align === "right") classes += " c-figure--right";
+
+    const trimmed = content.trim();
+
+    return `<figure class="${classes}">
+${trimmed.startsWith("!") ? md.renderInline(trimmed) : trimmed}
+${
+  caption ? `<figcaption class="c-figure__caption">${caption}</figcaption>` : ""
+}
+</figure>`;
+  });
 };
---
+++ _data/site.js
@@ -0,0 +1,11 @@
+const isDev = process.env.ELEVENTY_ENV === "development";
+
+const baseUrl = isDev ? `localhost:8080` : `https://www.gregtyler.co.uk/`;
+
+const site = {
+  title: "Your site title",
+  description: "Your site description",
+  baseUrl,
+};
+
+module.exports = site;
